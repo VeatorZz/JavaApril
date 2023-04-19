93f02311513a26425485b9cdeeeb6c7744d1466d
VeatorZz <Veator_LMY@163.com>
2023-04-19 19:42:57 +0800

    Update asd.java

--- untitled/src/asd.java
+++ untitled/src/asd.java
@@ -1,10 +1,42 @@
-class asd {
-    class Circle {
-        private double radius;
+ public class TreeNode {
+     int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+ *    TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ret =new ArrayList<>();
+        if(root == null) return ret ;
 
-        public Circle(double radius) {
-            radius = radius;
+        Queue<TreeNode> queue =new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int  size = queue.size();
+            List<Integer> list =new ArrayList<>();
+            while (size != 0){
+                TreeNode cur =queue.poll();
+                list.add(cur.val);
+                size--;
+                if(cur.left !=null){
+                    queue.offer(cur.left);
+                }
+                if(cur.right !=null){
+                    queue.offer(cur.right);
+                }
+            }
+            ret.add(list);
         }
+        return ret;
     }
-    }
+
+}
 
