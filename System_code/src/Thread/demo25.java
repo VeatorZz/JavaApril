e93b3ae46ef4b1edd7d15d0f9293270d16f8ee65
VeatorZz <Veator_LMY@163.com>
2023-05-03 12:25:44 +0800

    1

--- System_code/src/Thread/demo25.java
+++ System_code/src/Thread/demo25.java
@@ -1,14 +1,52 @@
 package Thread;
-class MyThreadPool{
-    //核心方法线程池里插入任务
-    public static void submit(Runnable runnable){
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+//咱们自己写的线程池类
+//简单实现成,固定10个线程的线程池
+class MyThreadPool{
+    //这个队列就是"任务队列"把当前线程池要完成的任务都放到这个队列中
+    //再由线程池内部的工作线程负责完成他们
+    private BlockingQueue<Runnable> queue=new LinkedBlockingQueue<>();
+    public  void submit(Runnable runnable){
+        try{
+            queue.put(runnable);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public MyThreadPool(int n){
+        //构造方法中,就需要创建一些线程,让这些线程负责完成上述执行任务的工作!!!
+        for (int i = 0; i < n; i++) {
+            Thread t=new Thread(()->{
+               while(Thread.currentThread().isInterrupted()){
+                   try{
+                      Runnable runnable= queue.take();
+                      runnable.run();
+                   } catch (InterruptedException e) {
+                       e.printStackTrace();
+                       break;
+                   }
+               }
+            });
+            t.start();
+        }
     }
 }
 public class demo25 {
 
 
     public static void main(String[] args) {
+        MyThreadPool myThreadPool=new MyThreadPool(10);
+        for (int i = 0; i < 100; i++) {
+            myThreadPool.submit(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("hello");
+                }
+            });
+        }
 
     }
 }
---
+++ System_code/src/Thread/demo26.java
@@ -0,0 +1,11 @@
+package Thread;
+
+public class demo26 {
+    private  synchronized static  void  func(){
+        //进行一些多线程操作
+        func1();
+    }
+    private synchronized static void  func1(){
+
+    }
+}
